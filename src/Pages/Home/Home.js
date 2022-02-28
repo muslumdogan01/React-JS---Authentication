1c9cab511404896670c48389093adebfe9902e23
muslumdogan01 <muslumdogan01@gmail.com>
2022-02-28 15:16:33 +0300

    fix SingUp

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Home = (props) => {
   return <div>
-    <h1>Welcome to test authentication. Please <Link to={"/SingUp"}><a>SingUp</a></Link></h1>
+    <h1>Welcome to test authentication. Please <Link to={"/SingUp"}><a>SingUp.</a></Link></h1>
   </div>;
 };
 
